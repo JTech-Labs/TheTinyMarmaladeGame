92c1ac4d5231e682306c4a7d607ab55ab8f83442
U+1F596 <101731484+JTech-Labs@users.noreply.github.com>
2025-02-06 09:38:13 +0000

    Add header to args.c

--- src/args.c
+++ src/args.c
@@ -1,7 +1,23 @@
+/*
+This is the main file for The Tiny Marmalade Game.
+Please see the README.md file that should have been distributed with this source code (it is probably in the root of the project).
+
+This file is part of The Tiny Marmalade Game.
+The Tiny Marmalade Game is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+The Tiny Marmalade Game is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with The Tiny Marmalade Game's source code (It resides in the LICENCE file at the root of the project). If not, see <https://www.gnu.org/licenses/>. 
+
+(c) 2025 JTech-Labs
+*/
+
+// Headers
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+
+// The header where the `theArgs` function is called from
+#include "theCommands.h"
 
 int theArgs(int argc, char *argv[]) {
   // Print the number of arguments passed to the program
